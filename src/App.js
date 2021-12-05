f49ff84bdcc289b56de6964cc3201d1b4805c783
YBogdan RADOI <radoi.office@gmail.com>
2021-12-05 17:43:38 +0100

    Creates POST request + Firebase DB

--- src/App.js
+++ src/App.js
@@ -1,39 +1,59 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async() => {
     setIsLoading(true);
     setError(null);
     try {
       // fetch() returns a response so we must await it
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://react-movie-http-95492-default-rtdb.europe-west1.firebasedatabase.app/movies.json");
       if (!response.ok) {
         // trigger catch block
         throw new Error("Something went wrong.");
       }
       // json() transform json to js object and returns a promise so we must await it
       const data = await response.json();
-
-      const tranformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(tranformedMovies);
+      let loadedMovies = [];
+      for (let key in data) {
+        loadedMovies.push({
+          id : key,
+          title : data[key].title,
+          openingText : data[key].openingText,
+          releaseDate : data[key].releaseDate
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
+  }, []);
+
+  // We need to use useCallback on the function dependency (objects are of reference type)
+  useEffect(() => {
+    fetchMoviesHandler();
+  },[fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    // returns a promise so we must await it
+    const response = await fetch("https://react-movie-http-95492-default-rtdb.europe-west1.firebasedatabase.app/movies.json",{
+      method: 'POST',
+      // stringify turns JS object or [] in JSON
+      body : JSON.stringify(movie),
+      headers : {
+        'Content-Type': 'application/json'
+      }
+    });
+    // json() transform json to js object and returns a promise so we must await it
+    const data = await response.json();
   }
 
   let content = <p>Found no movies.</p>;
@@ -48,6 +68,9 @@ function App() {
   }
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
